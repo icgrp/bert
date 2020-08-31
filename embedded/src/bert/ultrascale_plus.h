7ec85096877db7dc43fe6bb8bbe70116f9c76d70
matth2k <47065711+matth2k@users.noreply.github.com>
2020-08-30 21:00:32 -0400

    Update ultrascale_plus.h

--- embedded/src/bert/ultrascale_plus.h
+++ embedded/src/bert/ultrascale_plus.h
@@ -5,13 +5,5 @@
 // bitstream ctrl commands
 #define WORDS_BETWEEN_FRAMES 7
 #define WORDS_AFTER_FRAMES 54
-#define WORDS_BEFORE_FRAMES 194
+#define WORDS_BEFORE_FRAMES 196
 
-// Comment out the one you want to benchmark
-// One at a time
-
-//#define TIME_TRANSLATION
-#define TIME_BERT
-//#define TIME_DMA
-
-#define USE_WE
