d5c3f6251274954a0b39f7be1039b1897a7d038c
Matthew Hofmann <matth2k@seas.upenn.edu>
2020-12-15 01:19:21 -0500

    script to dl and copy 2019 xil libraries

---
+++ docs/tutorials/huffman/downloadLibraries.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+tempdir=temp
+
+if [ -z "$XIL_SDK_DIR" ]
+then
+    echo "XIL_SDK_DIR is not set"
+    echo "It is the path to to the SDK installation. Ends with 2018.3"
+    echo "Example: export XIL_SDK_DIR=Xilinx/SDX/SDK/2018.3"
+else
+    echo "Checking $XIL_SDK_DIR exists"
+    path=$(realpath -se $XIL_SDK_DIR)
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+    wget https://github.com/Xilinx/embeddedsw/archive/xilinx-v2019.2.tar.gz
+    mkdir $tempdir
+    tar -xf xilinx-v2019.2.tar.gz -C "temp"
+    path=$(realpath -se $XIL_SDK_DIR)
+    cp -r $tempdir/embeddedsw-xilinx-v2019.2/lib/sw_services/xilfpga $path/data/embeddedsw/lib/sw_services/xilfpga_v5_1
+    cp -r $tempdir/embeddedsw-xilinx-v2019.2/lib/sw_services/xilsecure $path/data/embeddedsw/lib/sw_services/xilsecure_>    cp -r $tempdir/embeddedsw-xilinx-v2019.2/lib/bsp/standalone $path/data/embeddedsw/lib/bsp/standalone_v7_1
+
+    echo "Done copying files."
+    echo "Restart your SDK now."
+fi
