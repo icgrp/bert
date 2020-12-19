6260c065f6ba09b8c34d8b9299fb918b348c108c
Matt <matth2k@seas.upenn.edu>
2020-12-19 10:10:26 +0000

    Added first comments for code

--- docs/tutorials/huffman/sw_huffman/hellobert.c
+++ docs/tutorials/huffman/sw_huffman/hellobert.c
@@ -6,26 +6,29 @@
 #include "bzlib_private.h"
 #include "mydesign.h"
 
-
+// Word-length of logical memories for the results and histogram
 #define HIST_LEN 256
 #define RESULT_LEN 512
+// TO DEBUG: once bert-gen IDCODE finding is fixed, remove this
 #define U96_IDCODE 0x04A42093
 
-
+// Define print for embedded platform
 #define PRINT xil_printf
 
-// debug -- maybe need to get AXI reads to work under -O3
+// TO DEBUG: maybe need to get AXI reads to work under -O3
 //#define VERBOSE_AXI 1
 #undef VERBOSE_AXI
 
+// Pointers to BRAMs exposed to AXI busses
 unsigned int * raw =     (unsigned int *)0xA0001000;
 unsigned int * huff =    (unsigned int *)0xA0003000;
 unsigned int * hist =    (unsigned int *)0xA0002000;
 unsigned int * results = (unsigned int *)0xA0004000;
+// Pointer to control register to reset Huffman encoder
 unsigned int * huffRegs = (unsigned int *)0xA0000000;
 
 
-// double the space to decrease chance of out of bounds write
+// TO DEBUG: double the space to decrease chance of out of bounds write
 uint64_t bert_results[1024*2];
 uint64_t bert_hist[2048*2];
 uint64_t bert_huff[512*2];
@@ -33,9 +36,13 @@ uint64_t axi_hist[2048*2];
 uint64_t axi_results[1024*2];
 uint64_t bert_raw[1024];
 
+/*
+* xilfpga object that needs to be initiliazed.
+* Contains information about the device and pointers to functions that read/write bitstreams
+*/
 XFpga XFpgaInstance = {0U};
 
-
+// Method that copies the state of memories over AXI into memory buffers
 void extractAxi(int hlen, int rlen) {
     PRINT("Read histogram through axi...\n");
     for (int i=0; i<hlen; i++) {
@@ -56,6 +63,7 @@ void extractAxi(int hlen, int rlen) {
     PRINT("  finish extractAxi\n");
 }
 
+// Method that copies the state of memories using BERT into memory buffers
 void extractBert(int raw, int hist, int result) {
 	// Clear just in case, remove later
 	for (int i = 0; i < HIST_LEN; i++) {
@@ -72,12 +80,13 @@ void extractBert(int raw, int hist, int result) {
 	  bert_read(raw,bert_raw,&XFpgaInstance);
 }
 
-
+// For debugging state of Huffman encoder
 void readRegs() {
     for (int i=0;i<4;i++)
       PRINT("  Reg[%d] = %d\n", i, *(huffRegs+i));
 }
 
+// Reset the Huffman encoder. This means updating the results memory with a new encoding using the current input memory and Huffman table memory.
 void clrHuff() {
     PRINT("Resetting huffman...\n");
     *huffRegs = 1;
@@ -101,13 +110,16 @@ void waitHuff() {
     PRINT("\n  Success, received done in %d tries\n", n);
 }
 
-
+/*
+* Compares the read results from AXI and BERT to verify BERT is working.
+* It also prints the number of values read as 0 to prevent false positives (like when both AXI and BERT read 0s on a failed BERT write).
+*/
 void compare(int hlen, int rlen)
 {
   int histerr=0;
   int resulterr=0;
-  int histzeros = 0;
-  int resultzeros = 0;
+  int histzeros=0;
+  int resultzeros=0;
   for(int i=0; i<hlen; i++)
     if (axi_hist[i]!=bert_hist[i])
       {
@@ -138,6 +150,10 @@ void compare(int hlen, int rlen)
     }
 }
 
+/*
+* Compute a Huffman encoding table on the PS using the histogram memory read over BERT
+* Assumes bert_read was already called on the histogram memory.
+*/
 void recompute_huffman(int *result_code) {
   UChar len[256];
   Int32 code[256];
@@ -147,11 +163,9 @@ void recompute_huffman(int *result_code) {
   // setup result_code;
   for (int i=0;i<256;i++)
     {
-      // note from encoder.sv
-          // Split apart the incoming character
-      //assign huffmanLen = code[19:16];
-      //assign huffmanCode = code[15:0];
-      result_code[i]=(len[i]<<15) | code[i]; // Is this supposed to be code[i]? code[] is type Int32*
+      // Bits [19:16] are the encoding length
+      // Bits [15:0] are allocated to the encoding
+      result_code[i]=(len[i]<<15) | code[i];
     }
 }
 
@@ -159,31 +173,31 @@ void recompute_huffman(int *result_code) {
 
 int main() {
 
-	PRINT("Testing huffman with bert compressed\n");
+    PRINT("Testing huffman with bert compressed\n");
+    // Get indices of logical memories by name in hardware design
+    int mem_input_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/PRODUCER/inst/HUFFMAN/PRODUCER/rawTextMem",NUM_LOGICAL);
+    int mem_huffman_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/ENCODER/inst/HUFFMAN/ENCODER/huffmanMem",NUM_LOGICAL);
+    int mem_hist_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/ENCODER/HIST/histMem",NUM_LOGICAL);
+    int mem_result_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/CONSUMER/resultsMem",NUM_LOGICAL);
 
-	int mem_input_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/PRODUCER/inst/HUFFMAN/PRODUCER/rawTextMem",NUM_LOGICAL);
-	int mem_huffman_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/ENCODER/inst/HUFFMAN/ENCODER/huffmanMem",NUM_LOGICAL);
-	int mem_hist_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/ENCODER/HIST/histMem",NUM_LOGICAL);
-	int mem_result_slot=logical_memory_slot("design_1_i/top_0/inst/HUFFMAN/CONSUMER/resultsMem",NUM_LOGICAL);
 
-
-	PRINT("input=%d huffman=%d hist=%d result=%d\n",mem_input_slot,mem_huffman_slot,mem_hist_slot,mem_result_slot);
-	if (mem_input_slot<0) {
-		PRINT("unable to find specified input slot\n");
-		return -1;
-	}
-	if (mem_huffman_slot<0) {
-		PRINT("unable to find specified huffman encoder slot\n");
-		return -1;
-	}
-	if (mem_hist_slot<0) {
-		PRINT("unable to find specified histogram slot\n");
-		return -1;
-	}
-	if (mem_result_slot<0) {
-		PRINT("unable to find specified result slot\n");
-		return -1;
-	}
+    PRINT("Memory indices: input=%d huffman=%d hist=%d result=%d\n",mem_input_slot,mem_huffman_slot,mem_hist_slot,mem_result_slot);
+    if (mem_input_slot<0) {
+        PRINT("unable to find specified input slot\n");
+        return -1;
+    }
+    if (mem_huffman_slot<0) {
+        PRINT("unable to find specified huffman encoder slot\n");
+        return -1;
+    }
+    if (mem_hist_slot<0) {
+        PRINT("unable to find specified histogram slot\n");
+        return -1;
+    }
+    if (mem_result_slot<0) {
+        PRINT("unable to find specified result slot\n");
+        return -1;
+    }
     readback_Init(&XFpgaInstance, U96_IDCODE);
 
     uint64_t new_code[512];
@@ -195,11 +209,14 @@ int main() {
 
 
     /*
-     * INITIAL READ
+     * 0. INITIAL READ OF LOGICAL MEMORIES
      */
-    xil_printf("\r\nINITIAL READ\r\n");
+    xil_printf("\r\n0. INITIAL READ OF LOGICAL MEMORIES\r\n");
+    // Read encoding results and histogram over AXI
     extractAxi(HIST_LEN, RESULT_LEN);
+    // Read encoding results and histogram over BERT
     extractBert(mem_input_slot,mem_hist_slot,mem_result_slot);
+    // Check that they agree
     compare(HIST_LEN, RESULT_LEN);
     xil_printf("result[1] = %x\r\n", axi_results[1]);
     xil_printf("result[2] = %x\r\n", axi_results[2]);
@@ -207,38 +224,55 @@ int main() {
 
 
     /*
-     * WRITE NEW ENCODING TO HUFFMAN THROUGH BERT
+     * 1. TEST WRITING NEW ENCODING TO HUFFMAN THROUGH BERT
      */
-    xil_printf("\r\nWRITE NEW ENCODING TO HUFFMAN THROUGH BERT USING huffman.c\r\n");
+    xil_printf("\r\n1. TEST WRITING NEW ENCODING TO HUFFMAN THROUGH BERT USING huffman.c\r\n");
+    // Use the histogram reading from step 0 to compute a Huffman table on the PS
     recompute_huffman(new_code);
+    // Write that encoding stored in int[] new_code to the embedded memory
     bert_write(mem_huffman_slot,new_code,&XFpgaInstance);
+    // Reset the Huffman encoder so that it will use the new encodings
     clrHuff();
     waitHuff();
+    // Read encoding results and histogram over AXI
     extractAxi(HIST_LEN, RESULT_LEN);
+    // Read encoding results and histogram over BERT
     extractBert(mem_input_slot,mem_hist_slot,mem_result_slot);
+    // Check that they agree
     compare(HIST_LEN, RESULT_LEN);
     xil_printf("result[1] = %x\r\n", axi_results[1]);
     xil_printf("result[2] = %x\r\n", axi_results[2]);
     xil_printf("result[3] = %x\r\n", axi_results[3]);
 
     /*
-     * WRITE IDENTITY ENCODING TO HUFFMAN THROUGH BERT
-     * WRITE ASCENDING RAW INPUT
+     * 2. TEST WRITING IDENTITY ENCODING TO HUFFMAN THROUGH BERT
+     *    TEST WRITING ASCENDING RAW INPUT
      */
-    xil_printf("\r\nWRITE IDENTITY ENCODING TO HUFFMAN THROUGH BERT\r\n");
-    xil_printf("WRITE ASCENDING RAW INPUT\r\n");
+    xil_printf("\r\n2. TEST WRITING IDENTITY ENCODING TO HUFFMAN THROUGH BERT\r\n");
+    xil_printf("   TEST WRITING ASCENDING RAW INPUT\r\n");
+    // Create an identity encoding and write ascending values to the input memory
     for (int i = 0; i < 512; i++) {
-    	new_code[i] = (8 << 16) | (i % 256);
-    	bert_raw[i] = (i % 256);
-    	bert_raw[i+512] = (i % 256);
+      char c = i % 256;
+      // Encoding is the value itself. Encoding size is always 8b
+    	new_code[i] = (8 << 16) | (uint64_t) c;
+      // Write char to memory. Raw input is twice as large as the histogram
+    	bert_raw[i] = (uint64_t) c;
+    	bert_raw[i+512] = (uint64_t) c;
     }
+    // Write that encoding stored in int[] new_code to the embedded memory
     bert_write(mem_huffman_slot,new_code,&XFpgaInstance);
+    // Write the ascending input in int[] bert_raw to the embedded memory
     bert_write(mem_input_slot,bert_raw,&XFpgaInstance);
+    // Reset the Huffman encoder so that it will use the new encodings
     clrHuff();
     waitHuff();
+    // Read encoding results and histogram over AXI
     extractAxi(HIST_LEN, RESULT_LEN);
+    // Read encoding results and histogram over BERT
     extractBert(mem_input_slot,mem_hist_slot,mem_result_slot);
+    // Check that they agree
     compare(HIST_LEN, RESULT_LEN);
+    // The results should be different now.
     xil_printf("result[1] = %x\r\n", axi_results[1]);
     xil_printf("result[2] = %x\r\n", axi_results[2]);
     xil_printf("result[3] = %x\r\n", axi_results[3]);
