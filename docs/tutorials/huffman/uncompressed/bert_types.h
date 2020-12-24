2a155a3ece81bd0d9e4a59a63d81c4f97d397132
Andre' DeHon <andre@seas.upenn.edu>
2020-12-23 20:27:05 -0500

    setup for uncompressed for timing comparison

---
+++ docs/tutorials/huffman/uncompressed/bert_types.h
@@ -0,0 +1,30 @@
+struct frame_range {
+   int first_frame;
+   int len;
+};
+
+struct bit_loc {
+   int frame; // can probably be less than 32b
+   short int bit_loc; // at most 3232 bits in frame, so only need 11b
+};
+  
+struct logical_memory {
+   int nframe_ranges;
+   int wordlen;
+   int words;
+  int replicas;
+   struct frame_range *frame_ranges;
+   struct bit_loc *bit_locations;
+};
+
+struct frame_range_offset {
+  int frame_base;
+  int len;
+  int has_write;
+  int offset;
+};
+
+  struct frame_set {
+  int num_ranges;
+  struct frame_range_offset *ranges;
+};
