abf7b2a49d10aaf8f8da49278c0fbfa5d1e20bb3
zhiyaot <zytang1202@gmail.com>
2021-03-05 13:27:24 -0500

    adding stable bert_gen refactored to bert folder

--- host_tools/bert_gen/include/fpga_helper.h
+++ host_tools/bert_gen/include/fpga_helper.h
@@ -8,17 +8,23 @@
 #include <list>
 #include <map>
 #include <vector>
+#include <set>
 #include "fpga_type.h"
+#include "fpga_err.h"
 
 #ifndef BERT_GEN_FPGA_HELPER_H
 #define BERT_GEN_FPGA_HELPER_H
 
+using namespace bertType;
+using namespace std;
+
 const int BRAM_SIZE = 36864;
 
-extern int minFrame[6][3];
+uint64_t calcBitPosition_generic(uint32_t x_pos, uint32_t y_pos, uint32_t bit_num, uint32_t bramType, uint32_t yMax);
 
-uint64_t calc_bit_pos_ultra96(uint32_t x_pos, uint32_t y_pos, uint32_t bit_num, uint32_t type);
+void calcNFrameRanges_generic(set<uint32_t> &allFrameAddr, list <pair<uint32_t, uint32_t>> &foundRanges,
+                              fpga_PL &XfpgaInstance);
 
-void calc_nframe_range(std::list<unique_ptr<bram>>& all_logical, vector<vector<int>> &range_mark);
+void findPart(fpga_PL &XfpgaInstance, char* part);
 
 #endif //BERT_GEN_FPGA_HELPER_H
