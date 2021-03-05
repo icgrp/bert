8c402ef4c2dd0db1209850ffa02b4195d3546337
Matt H <47065711+matth2k@users.noreply.github.com>
2021-03-04 21:15:38 -0500

    Update 7series.h

--- embedded/src/bert/7series.h
+++ embedded/src/bert/7series.h
@@ -1,6 +1,6 @@
 #define WORDS_PER_FRAME 101
 #define FRAMES_PER_BRAM 128
-// Likely wrong for 7 series
+// Maybe wrong for 7 series. Haven't tried reading back through PCAP yet
 #define DATA_DMA_OFFSET 0x1FCU
 
 // bitstream ctrl commands
@@ -8,7 +8,7 @@
 #define WORDS_AFTER_FRAMES 54
 #define WORDS_BEFORE_FRAMES 196
 
-// for ultrascale+
+// for 7 series
 #define WE_BITS_PER_FRAME 10
 
 extern int bitlocation[10]; // write enables
