32b8f4142c74c8276e5cfa4ac5ffe1fdaf22a951
Matthew Hofmann <matth2k@seas.upenn.edu>
2021-04-09 16:31:14 -0400

    Add flags for vitis and sdk, prompt use if not specificed

--- docs/tutorials/huffman/copyappfiles.py
+++ docs/tutorials/huffman/copyappfiles.py
@@ -14,7 +14,26 @@ bertDir = "../../.."
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("workspaceDir", help='Directory which contains the SDK/Vitis workspace. The tutorial refers to this as WORK and is where provided files are copied to.  Example: /home/steven/myProject')
+    parser.set_defaults(use_vitis=None)
+    parser.add_argument('-v', '--vitis', dest='use_vitis', action='store_true')
+    parser.add_argument('-s', '--sdk', dest='use_vitis', action='store_false')
     args = parser.parse_args()
+
+    if args.use_vitis is None:
+        userinput = int(input("Enter 1 for SDK\nEnter 2 for Vitis ")) 
+        args.use_vitis = userinput == 2
+    
+    
+    if args.use_vitis:
+        hwDir = "hw_huffman_vitis"
+        swFile = "hellobert_vitis.c"
+        blacklistFile = "hellobert_sdk.c"
+        print("Copying files for vitis...\n")
+    else:
+        hwDir = "hw_huffman_sdk"
+        swFile = "hellobert_sdk.c"
+        blacklistFile = "hellobert_vitis.c"
+        print("Copying files for sdk...\n")
 
     bertDir = pathlib.Path(bertDir).resolve()
     assert os.path.isdir(str(bertDir)), "Cannot find BERT repo directory: " + str(bertDir) + "\n Run in same directory as script"
@@ -24,7 +43,7 @@ if __name__ == "__main__":
     bertSWHuffmanDir = bertDir / "docs" / "tutorials" / "huffman" / "sw_huffman" 
     assert os.path.isdir(str(bertSWHuffmanDir)), "Cannot find directory: " + str(bertSWHuffmanDir)
     # Does not matter where we pull mydesign.c/h from (sdk vs vitis)
-    bertHWHuffmanDir = bertDir / "docs" / "tutorials" / "huffman" / "hw_huffman_sdk" 
+    bertHWHuffmanDir = bertDir / "docs" / "tutorials" / "huffman" / hwDir 
     assert os.path.isdir(str(bertSWHuffmanDir)), "Cannot find directory: " + str(bertHWHuffmanDir)
 
     
@@ -32,13 +51,13 @@ if __name__ == "__main__":
     workDir = pathlib.Path(args.workspaceDir).resolve()
     assert os.path.isdir(str(workDir)), "Cannot find workspace directory: " + args.workspaceDir
     # Double check for SDKWorkspace default directory created by SDK on Linux
-    if not os.path.isdir(str(workDir / prjName)):
+    if not os.path.isdir(str(workDir / prjName)) and args.use_vitis is False:
         for dir in workDir.iterdir():
             if dir.name == "SDKWorkspace":
                 workDir = workDir / "SDKWorkspace"
                 break
     appSrcDir = workDir / prjName / "src"
-    assert os.path.isdir(str(appSrcDir)), "Cannot find project src directory: " + str(appSrcDir) + "\n Check project name. The project name can be adjust by editing the script."
+    assert os.path.isdir(str(appSrcDir)), "Cannot find project src directory: " + str(appSrcDir) + "\n Check project name and workspace directory. The project name can be adjusted by editing the script."
 
     print("Step 1. Copying BERT lib files from '{}' to \n                           '{}'".format(bertSrcDir, appSrcDir))
     for name in glob.glob(str(bertSrcDir / '*')):
@@ -51,5 +70,9 @@ if __name__ == "__main__":
         shutil.copy2(name, str(appSrcDir))
     print("Step 3. Copying app files from '{}' to \n                           '{}'".format(bertSWHuffmanDir, appSrcDir))
     for name in glob.glob(str(bertSWHuffmanDir / '*')):
-        print("    Copying file: {}".format(name))
-        shutil.copy2(name, str(appSrcDir))
+        if swFile in name:
+            shutil.copy2(name, str(appSrcDir / 'hellobert.c'))
+            print("    Copying file: {}".format(name))
+        elif blacklistFile not in name:
+            shutil.copy2(name, str(appSrcDir))
+            print("    Copying file: {}".format(name))
