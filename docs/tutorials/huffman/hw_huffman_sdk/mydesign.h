d167fc1aa3c3e71e451e0b923db84d055d5e6e5a
Andre DeHon <andre@acm.org>
2021-04-17 14:19:19 -0400

    try started v3 mydesign

--- docs/tutorials/huffman/hw_huffman_sdk/mydesign.h
+++ docs/tutorials/huffman/hw_huffman_sdk/mydesign.h
@@ -1,8 +1,10 @@
-#include "bert_types.h"
+#include "compressed_bert_types.h"
 
 #define NUM_LOGICAL 4
 
 #define PART_ID 0x04A42093
+
+#define IDCODE 0x77865107
 
 #define MEM_0 0
 #define MEM_1 1
@@ -10,4 +12,4 @@
 #define MEM_3 3
 
 extern const char * logical_names[NUM_LOGICAL];
-extern struct logical_memory logical_memories[NUM_LOGICAL];
+extern struct compressed_logical_memory logical_memories[NUM_LOGICAL];
